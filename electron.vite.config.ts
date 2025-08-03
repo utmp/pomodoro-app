fd018d61a8884eebad85aec494222974c4dd9c4a
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-08-03 19:38:33 +0300

    installed tailwindcss

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -15,6 +15,9 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [
+      react(),
+      tailwindcss()
+    ]
   }
 })
