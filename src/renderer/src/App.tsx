011bb2e43fd181776ee2a5c1c1d0a7c8f354ee71
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-08-04 13:59:35 +0300

    installed shadcn-ui

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,11 +1,19 @@
-
-function App(): React.JSX.Element {
-
+import { useState } from "react"
+import { Button } from "./components/ui/button"
+function Likes(){
+  const [likes,setLikes] = useState(0)
+  const handleClick = ()=>{
+    setLikes(likes+1)
+  }
   return (
-    <>
-     <h2 className="text-3xl font-bold underline">Pomodoro Timer</h2>
-    </>
+   <Button onClick={handleClick} variant="destructive">Likes: {likes}</Button>
+  
   )
+}
+function App() {
+ return(
+  <Likes></Likes>
+ )
 }
 
 export default App
